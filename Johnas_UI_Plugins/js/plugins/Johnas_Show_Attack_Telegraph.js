75ad5c6a6c68137843a6a5e6b73f73f8dd25df33
Johnas Wong <johnas@ualberta.ca>
2020-10-02 14:59:15 -0600

    With Shaz's TileChanger plugin, can change tiles through Show_Telegraph plugin. Just need to somehow get current tileID of affected tiles

--- Johnas_UI_Plugins/js/plugins/Johnas_Show_Attack_Telegraph.js
+++ Johnas_UI_Plugins/js/plugins/Johnas_Show_Attack_Telegraph.js
@@ -16,7 +16,12 @@
  /**
   * TO DO LIST:
 
-	- 
+    - Find the tileID of the tiles to be replaced
+    
+    - Put in waits after turning tile red before turning it back to old tile
+
+    - Extra info section in the top comment blocks to say "Shaz's Shaz_TileChanger.js"
+      is needed to be in the plugins
   */
 
 /**
@@ -46,20 +51,6 @@
 
 
 
-// // 74 is key "J"
-// Input.keyMapper[ "74" ] = "activate";
-
-// _alias_scene_map_update = Scene_Map.prototype.update;
-// Scene_Map.prototype.update = function() {
-//     _alias_scene_map_update.call( this );
-//     if( Input.isTriggered( "activate" ) )
-//     {
-//         console.log( $gameMap.tileCodeAt( 0, 0, 1 ) );
-//         console.log( "pressed j" );
-//     }
-// };
-
-
 
 ( function() 
 {  
@@ -80,28 +71,19 @@
 
 
 
-            console.log( $gameMap.data[0,0,0] );
+            /**
+             * $game_map.tile_id(x, y, layer)
+             * Should return the tileID of the selected tile
+             */
+            console.log( $game_map.tile_id(x, y, layer) );
             // $game_map.data[11,5,0] = 2816;
 
-
-            /**
-                 Game_Map.prototype.changeTile = function(args) {
-                    var x = eval(args.shift());
-                    var y = eval(args.shift());
-                    var z = eval(args.shift());
-                    var tileId = eval(args.shift());
-                    var mapLoc = this.calcIndex($dataMap, x, y, z);
-            
-                    $dataMap.data[mapLoc] = tileId;
-                    $gameSystem.saveMapTile(mapLoc, tileId);
-                    $gameTemp.setMapDataRefresh(true);
-                };
-             */
 
             var myInterpreter = new Game_Interpreter()
             myInterpreter.pluginCommand('ChangeTile', ['3', '2', '0', '4']);
             /**
-             * ChangeTile 3 3 0 1
+             * ChangeTile 4 3 0 1
+             * Changetile( x, y, z, tileID );
              */
 
 
