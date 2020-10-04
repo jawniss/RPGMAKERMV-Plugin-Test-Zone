b1d65a8d6fde99b4f7376c50fccea7b9a18cf4ff
Johnas Wong <johnas@ualberta.ca>
2020-10-04 12:42:42 -0600

    Prototype for attack telegraphing works, can draw affected tiles and redraw original tile

--- Johnas_UI_Plugins/js/plugins/Johnas_Show_Attack_Telegraph.js
+++ Johnas_UI_Plugins/js/plugins/Johnas_Show_Attack_Telegraph.js
@@ -16,7 +16,14 @@
  /**
   * TO DO LIST:
 
-    - Find the tileID of the tiles to be replaced
+    - Make different cases for the different enemy attacks
+
+    - Find/think out how the other files will access the functions in here
+      - I'm thinking do via plugin command, 
+        var myInterpreter = new Game_Interpreter()
+        myInterpreter.pluginCommand('Show_Attack', ['x', 'y', 'z', 'type_of_attack']);
+
+      - Have to learn how to do the args thing
     
     - Put in waits after turning tile red before turning it back to old tile
 
@@ -41,64 +48,21 @@
  */
 
 
-// function Game_Map() {
-//   this.initialize.apply(this, arguments);
-// }
-
-// Game_Map.prototype.initialize = function() {
-//   this._interpreter = new Game_Interpreter();
-  // this._mapId = 0;
-  // this._tilesetId = 0;
-  // this._events = [];
-  // this._commonEvents = [];
-  // this._vehicles = [];
-  // this._displayX = 0;
-  // this._displayY = 0;
-  // this._nameDisplay = true;
-  // this._scrollDirection = 2;
-  // this._scrollRest = 0;
-  // this._scrollSpeed = 4;
-  // this._parallaxName = '';
-  // this._parallaxZero = false;
-  // this._parallaxLoopX = false;
-  // this._parallaxLoopY = false;
-  // this._parallaxSx = 0;
-  // this._parallaxSy = 0;
-  // this._parallaxX = 0;
-  // this._parallaxY = 0;
-  // this._battleback1Name = null;
-  // this._battleback2Name = null;
-// };
-
-// Game_Map.prototype.setup = function(mapId) {
-//   if (!$dataMap) {
-//       throw new Error('The map data is not available');
-//   }
-//   this._mapId = mapId;
-//   this._tilesetId = $dataMap.tilesetId;
-//   this._displayX = 0;
-//   this._displayY = 0;
-//   this._needsRefresh = false;
-// };
-
 ( function() 
 {  
 	var parameters = PluginManager.parameters('Show_Attack_Telegraph');
        
 
     // 74 is key "J"
-    Input.keyMapper[ "74" ] = "Change tiles";
-
+    Input.keyMapper[ "74" ] = "Attack Happening";
     // Have to watch out - _alias .. is used in hearts_working so have to
     // have different names
-    _alias_scene_map_update2 = Scene_Map.prototype.update;
+    _alias_scene_map_draw_attack_tiles = Scene_Map.prototype.update;
     Scene_Map.prototype.update = function() {
-        _alias_scene_map_update2.call( this );
-        if( Input.isTriggered( "Change tiles" ) )
+      _alias_scene_map_draw_attack_tiles.call( this );
+        if( Input.isTriggered( "Attack Happening" ) )
         {
             console.log( "The J button has been pressed" );
-
-
 
             /**
              * In rpg_objects.js there's the tileID function
@@ -107,15 +71,8 @@
             console.log( $gameMap.tileId( 0, 0, 0 ) );
             // $game_map.data[11,5,0] = 2816;
 
-
             console.log( "Tile ID at 1, 1, 0" );
             console.log( $gameMap.tileId( 1, 1, 0 ) );
-
-
-
-
-
-
 
             /**
              * Working function that works as "Plugin command"
@@ -131,14 +88,39 @@
             console.log( $gameMap.tileId( 3, 2, 0 ) );
 
             console.log( "Done the 'Pressed J' comand" );
-            
-
-
-
         }
     };
 
 
+
+
+    // 74 is key "K"
+    Input.keyMapper[ "75" ] = "Attack Finished Redraw OG";
+    // Have to watch out - _alias .. is used in hearts_working so have to
+    // have different names
+    _alias_scene_map_redraw_og_tiles = Scene_Map.prototype.update;
+    Scene_Map.prototype.update = function() {
+      _alias_scene_map_redraw_og_tiles.call( this );
+        if( Input.isTriggered( "Attack Finished Redraw OG" ) )
+        {
+            console.log( "The K button has been pressed" );
+
+            /**
+             * Working function that works as "Plugin command"
+             */
+            var myInterpreter = new Game_Interpreter()
+            myInterpreter.pluginCommand('ChangeTile', ['3', '2', '0', '2816']);
+            /**
+             * ChangeTile 4 3 0 1
+             * Changetile( x, y, z, tileID );
+             */
+
+            console.log( "Tile ID at 3, 2, 0" );
+            console.log( $gameMap.tileId( 3, 2, 0 ) );
+
+            console.log( "Done the 'Pressed K' comand" );
+        }
+    };
 
 
 })();  // dont touch this.
