55dd0788029b77fbcad94a1dbab384c0e1e033cd
Johnas Wong <johnas@ualberta.ca>
2020-10-04 12:36:05 -0600

    Prototype for attack telegraphing works, executes on button press

--- Johnas_UI_Plugins/js/plugins/Johnas_Show_Attack_Telegraph.js
+++ Johnas_UI_Plugins/js/plugins/Johnas_Show_Attack_Telegraph.js
@@ -22,6 +22,10 @@
 
     - Extra info section in the top comment blocks to say "Shaz's Shaz_TileChanger.js"
       is needed to be in the plugins
+
+    - MAJOR POTENTIAL ISSUE: DOING THE PLUGIN COMMAND WITH THE BUILT IN DEFAULT
+      RPG MAKER TILES, NOT SURE HOW IT WILL WORK WITH CUSTOM TILES THAT ARE IMPORTED
+      INTO THE GAME
   */
 
 /**
@@ -36,21 +40,46 @@
  * and upon execution it'll make the four adjacent tiles red
  */
 
-/**
- * Input.keyMapper[ "80" ] = "customMenu";
 
-    _alias_scene_map_update = Scene_Map.prototype.update;
-    Scene_Map.prototype.update = function() {
-        _alias_scene_map_update.call( this );
-        if( Input.isTriggered( "customMenu" ) )
-        {
-            SceneManager.push( Scene_CustomMenu );
-        }
-    };
- */
+// function Game_Map() {
+//   this.initialize.apply(this, arguments);
+// }
 
+// Game_Map.prototype.initialize = function() {
+//   this._interpreter = new Game_Interpreter();
+  // this._mapId = 0;
+  // this._tilesetId = 0;
+  // this._events = [];
+  // this._commonEvents = [];
+  // this._vehicles = [];
+  // this._displayX = 0;
+  // this._displayY = 0;
+  // this._nameDisplay = true;
+  // this._scrollDirection = 2;
+  // this._scrollRest = 0;
+  // this._scrollSpeed = 4;
+  // this._parallaxName = '';
+  // this._parallaxZero = false;
+  // this._parallaxLoopX = false;
+  // this._parallaxLoopY = false;
+  // this._parallaxSx = 0;
+  // this._parallaxSy = 0;
+  // this._parallaxX = 0;
+  // this._parallaxY = 0;
+  // this._battleback1Name = null;
+  // this._battleback2Name = null;
+// };
 
-
+// Game_Map.prototype.setup = function(mapId) {
+//   if (!$dataMap) {
+//       throw new Error('The map data is not available');
+//   }
+//   this._mapId = mapId;
+//   this._tilesetId = $dataMap.tilesetId;
+//   this._displayX = 0;
+//   this._displayY = 0;
+//   this._needsRefresh = false;
+// };
 
 ( function() 
 {  
@@ -58,27 +87,39 @@
        
 
     // 74 is key "J"
-    Input.keyMapper[ "74" ] = "activate";
+    Input.keyMapper[ "74" ] = "Change tiles";
 
     // Have to watch out - _alias .. is used in hearts_working so have to
     // have different names
     _alias_scene_map_update2 = Scene_Map.prototype.update;
     Scene_Map.prototype.update = function() {
         _alias_scene_map_update2.call( this );
-        if( Input.isTriggered( "activate" ) )
+        if( Input.isTriggered( "Change tiles" ) )
         {
-            console.log( "pressed j" );
+            console.log( "The J button has been pressed" );
 
 
 
             /**
-             * $game_map.tile_id(x, y, layer)
-             * Should return the tileID of the selected tile
+             * In rpg_objects.js there's the tileID function
              */
-            console.log( $game_map.tile_id(x, y, layer) );
+            console.log( "Tile ID at 0, 0, 0" );
+            console.log( $gameMap.tileId( 0, 0, 0 ) );
             // $game_map.data[11,5,0] = 2816;
 
 
+            console.log( "Tile ID at 1, 1, 0" );
+            console.log( $gameMap.tileId( 1, 1, 0 ) );
+
+
+
+
+
+
+
+            /**
+             * Working function that works as "Plugin command"
+             */
             var myInterpreter = new Game_Interpreter()
             myInterpreter.pluginCommand('ChangeTile', ['3', '2', '0', '4']);
             /**
@@ -86,9 +127,10 @@
              * Changetile( x, y, z, tileID );
              */
 
+            console.log( "Tile ID at 3, 2, 0" );
+            console.log( $gameMap.tileId( 3, 2, 0 ) );
 
-
-            console.log( "done" );
+            console.log( "Done the 'Pressed J' comand" );
             
 
 
