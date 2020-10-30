4ca72d8aa99bd1052e95ef1bd5d7e9047b990981
Johnas Wong <johnas@ualberta.ca>
2020-10-30 10:59:53 -0600

    Return to main menu yes/no, still breaks game, can't draw image

--- Johnas_UI_Plugins/js/plugins/Pause_Menu.js
+++ Johnas_UI_Plugins/js/plugins/Pause_Menu.js
@@ -15,6 +15,14 @@ Scene_Map.prototype.update = function()
 };
 
 
+Window_Base.prototype.drawPicture = function(filename, x, y) 
+{    
+    var bitmap = ImageManager.loadPicture(filename);    
+    this.contents.blt(bitmap, 0, 0, bitmap._canvas.width, bitmap._canvas.height, x, y);  
+};
+
+
+
 function Scene_CustomMenu() 
 {
     this.initialize.apply( this, arguments );
@@ -33,7 +41,7 @@ Scene_CustomMenu.prototype.initialize = function()
 Scene_CustomMenu.prototype.create = function() 
 {
     Scene_MenuBase.prototype.create.call( this );
-    this._customWindow = new Window_Custom( 0, 0, 320, 240 );
+    this._customWindow = new Window_Custom( 0, 200, 320, 240 );
     ImageManager.reserveFace( "Actor3" );
     ImageManager.reserveCharacter( "People1" );
     this.addWindow( this._customWindow );
@@ -108,8 +116,7 @@ Scene_CustomMenu.prototype.command4 = function()
 {
     if( this._customCommandWindow.visible ) this._customCommandWindow.activate();
     else this._customHorzCommandWindow.activate();
-    console.log( "Returning to main menu" );
-    SceneManager.goto( Scene_Title )
+    SceneManager.push( MainMenu_Yes_No_Menu );
 }
 
 
@@ -147,12 +154,20 @@ Window_Custom.prototype.initialize = function( x, y, width, height )
 Window_Custom.prototype.drawAllItems = function() 
 {
     this.contents.clear();
+    console.log( "drawalltimes" );
     this.drawText( $gameVariables.value( 2 ), 0, 0, this.width - this.padding * 2, "center" );
-    // this.drawIcon( 45, 48, 48 );
-    // this.drawFace( "Actor3", 5, 96, 96, 144, 144 );
-    // this.drawCharacter( "People1", 0, this.padding, this.padding + 96 );
-    // this.drawGauge( 0, 0, 100, 1, "#ff0000", "#00ff00" );
+    this.drawIcon( 45, 48, 48 );
+    this.drawFace( "Actor3", 5, 96, 96, 144, 144 );
+    this.drawCharacter( "People1", 0, this.padding, this.padding + 96 );
+    this.drawGauge( 0, 0, 100, 1, "#ff0000", "#00ff00" );
 }
+
+
+// Window_Custom.prototype.update = function() 
+// {
+//     console.log( "refreshing window_custom" );
+//     Window_Custom.prototype.update.call( this );
+// }
 
 
 function Window_CustomSelectable() 
@@ -397,7 +412,7 @@ Change_Timer_Length_Menu.prototype.command3 = function()
 Change_Timer_Length_Menu.prototype.start = function() 
 {
     Scene_MenuBase.prototype.start.call( this );
-    this._customWindow.drawAllItems();
+    // this._customWindow.drawAllItems();
     this._customSelectableWindow.refresh();
     this._customCommandWindow.refresh();
 };
@@ -604,14 +619,6 @@ Window_Custom_TimerHorzCommand.prototype.windowWidth = function()
 
 
 
-// // key P
-// Input.keyMapper["80"] = "customMenu";
-
-// _alias_scene_map_update = Scene_Map.prototype.update;
-// Scene_Map.prototype.update = function() {
-//     _alias_scene_map_update.call( this );
-//     if( Input.isTriggered( "customMenu" ) ) SceneManager.push( Restart_Yes_No_Menu );
-// };
 
 function Restart_Yes_No_Menu() 
 {
@@ -697,7 +704,7 @@ Restart_Yes_No_Menu.prototype.command2 = function()
 Restart_Yes_No_Menu.prototype.start = function() 
 {
     Scene_MenuBase.prototype.start.call( this );
-    this._customWindow.drawAllItems();
+    // this._customWindow.drawAllItems();
     this._customSelectableWindow.refresh();
     this._customCommandWindow.refresh();
 };
@@ -868,6 +875,280 @@ Window_Restart_Yes_NoHorzCommand.prototype.makeCommandList = function()
 
 
 Window_Restart_Yes_NoHorzCommand.prototype.windowWidth = function() 
+{
+    return Graphics.boxWidth;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function MainMenu_Yes_No_Menu() 
+{
+    this.initialize.apply( this, arguments );
+}
+
+MainMenu_Yes_No_Menu.prototype = Object.create( Scene_MenuBase.prototype );
+MainMenu_Yes_No_Menu.prototype.constructor = MainMenu_Yes_No_Menu;
+
+MainMenu_Yes_No_Menu.prototype.initialize = function() 
+{
+    Scene_MenuBase.prototype.initialize.call( this );
+};
+
+MainMenu_Yes_No_Menu.prototype.create = function() 
+{
+    Scene_MenuBase.prototype.create.call( this );
+    this._customWindow = new Window_MainMenu_Yes_No( 0, 0, 320, 240 );
+    ImageManager.reserveFace( "Actor3" );
+    ImageManager.reserveCharacter( "People1" );
+    this.addWindow( this._customWindow );
+    
+    this._customSelectableWindow = new Window_MainMenu_Yes_NoSelectable( 0, 0, 180, 180 );
+    this._customSelectableWindow.hide();
+    this._customSelectableWindow.select( 0 );
+    // this._customSelectableWindow.deactivate();
+    this._customSelectableWindow.setHandler( "ok", this.command1.bind( this ) );
+    this._customSelectableWindow.setHandler( "cancel", this.popScene.bind( this ) );
+    ImageManager.reserveFace( "Actor2" );
+    this.addWindow( this._customSelectableWindow );
+    
+    this._customCommandWindow = new Window_MainMenu_Yes_NoCommand( 0, 0 );
+    this._customCommandWindow.setHandler( "command1", this.command1.bind( this ) );
+    this._customCommandWindow.setHandler( "command2", this.command2.bind( this ) );
+    this.addWindow( this._customCommandWindow );
+    // this._customCommandWindow.deactivate();
+
+    this._customHorzCommandWindow = new Window_MainMenu_Yes_NoHorzCommand( 0, 0 );
+    this._customHorzCommandWindow.setHandler( "command1", this.command1.bind( this ) );
+    this._customHorzCommandWindow.setHandler( "command2", this.command2.bind( this ) );
+    this.addWindow( this._customHorzCommandWindow );
+    // this._customHorzCommandWindow.deactivate();
+
+    // this.customButton = new Sprite_CustomButton( ImageManager.loadBitmap( "img/pictures/", "pd", 0, true ) );
+    // this.addChild( this.customButton );
+}
+
+
+MainMenu_Yes_No_Menu.prototype.command1 = function() 
+{
+    if( this._customCommandWindow.visible ) this._customCommandWindow.activate();
+    else if( this._customHorzCommandWindow.visible ) this._customHorzCommandWindow.activate();
+    else this._customSelectableWindow.activate();
+    console.log( "Returning to main menu" );
+    SceneManager.goto( Scene_Title );
+}
+
+
+MainMenu_Yes_No_Menu.prototype.command2 = function() 
+{
+    if( this._customCommandWindow.visible ) this._customCommandWindow.activate();
+    else this._customHorzCommandWindow.activate();
+    this.popScene();
+}
+
+
+MainMenu_Yes_No_Menu.prototype.start = function() 
+{
+    Scene_MenuBase.prototype.start.call( this );
+    // this._customWindow.drawAllItems();
+    this._customSelectableWindow.refresh();
+    this._customCommandWindow.refresh();
+};
+
+
+MainMenu_Yes_No_Menu.prototype.update = function() 
+{
+    Scene_MenuBase.prototype.update.call( this );
+    if( Input.isTriggered( "cancel" ) ) this.popScene();
+}
+
+
+function Window_MainMenu_Yes_No() 
+{
+    this.initialize.apply( this, arguments );
+}
+
+
+Window_MainMenu_Yes_No.prototype = Object.create( Window_Base.prototype );
+Window_MainMenu_Yes_No.prototype.constructor = Window_MainMenu_Yes_No;
+
+Window_MainMenu_Yes_No.prototype.initialize = function( x, y, width, height ) 
+{
+    Window_Base.prototype.initialize.call( this, x, y, width, height );
+}
+
+
+Window_MainMenu_Yes_No.prototype.drawAllItems = function() 
+{
+    this.contents.clear();
+    this.drawText( $gameVariables.value( 2 ), 0, 0, this.width - this.padding * 2, "center" );
+    // this.drawIcon( 45, 48, 48 );
+    // this.drawFace( "Actor3", 5, 96, 96, 144, 144 );
+    // this.drawCharacter( "People1", 0, this.padding, this.padding + 96 );
+    // this.drawGauge( 0, 0, 100, 1, "#ff0000", "#00ff00" );
+}
+
+
+function Window_MainMenu_Yes_NoSelectable() 
+{
+    this.initialize.apply( this, arguments );
+}
+
+
+Window_MainMenu_Yes_NoSelectable.prototype = Object.create( Window_Selectable.prototype );
+Window_MainMenu_Yes_NoSelectable.prototype.constructor = Window_Selectable;
+
+Window_MainMenu_Yes_NoSelectable.prototype.initialize = function( x, y, width, height ) {
+    Window_Selectable.prototype.initialize.call( this, x, y, width, height );
+    this.refresh();
+    this.hide();
+}
+
+
+Window_MainMenu_Yes_NoSelectable.prototype.maxItems = function() 
+{
+    return 3;
+}
+
+
+Window_MainMenu_Yes_NoSelectable.prototype.maxPageRows = function() 
+{
+    return 2;
+}
+
+
+Window_MainMenu_Yes_NoSelectable.prototype.maxPageItems = function() 
+{
+    return this.maxPageRows() * this.maxCols();
+}
+
+
+Window_MainMenu_Yes_NoSelectable.prototype.drawItem = function( index ) 
+{
+    var itemRect = this.itemRect( index );
+    // this.drawFace( "Actor2", 3 + index, itemRect.x, itemRect.y, itemRect.width / 2, itemRect.height / 2 );
+}
+
+
+Window_MainMenu_Yes_NoSelectable.prototype.itemHeight = function() {
+    return( this.height - this.padding * 2 ) / this.maxPageRows();
+};
+
+
+function Window_MainMenu_Yes_NoCommand() 
+{
+    this.initialize.apply( this, arguments );
+}
+
+
+Window_MainMenu_Yes_NoCommand.prototype = Object.create( Window_Command.prototype );
+Window_MainMenu_Yes_NoCommand.prototype.constructor = Window_MainMenu_Yes_NoCommand;
+
+Window_MainMenu_Yes_NoCommand.prototype.initialize = function( x, y ) 
+{
+    Window_Command.prototype.initialize.call( this, x, y );
+    this.opacity = 0;
+} 
+
+
+Window_MainMenu_Yes_NoCommand.prototype.makeCommandList = function() 
+{
+    this.addCommand( "Yes, return to main menu!", "command1" );
+    this.addCommand( "No, don't return to main menu", "command2" );
+};
+
+
+Window_MainMenu_Yes_NoCommand.prototype.drawItem = function( index ) 
+{
+    var itemRect = this.itemRect( index );
+    // this.drawFace( "Actor2", 3 + index, itemRect.x, itemRect.y, 144, itemRect.height );
+    Window_Command.prototype.drawItem.call( this, index );
+}
+
+
+Window_MainMenu_Yes_NoCommand.prototype.windowWidth = function()
+{
+    return Graphics.boxWidth;
+}
+
+
+Window_MainMenu_Yes_NoCommand.prototype.windowHeight = function() 
+{
+    return Graphics.boxHeight;
+}
+
+
+Window_MainMenu_Yes_NoCommand.prototype.itemRect = function( index ) 
+{
+    var rect = {};
+    if( index == 0 ) 
+    {
+        rect.x = ( Graphics.boxWidth / 2 ) - ( 140 );
+        rect.y = 150;
+        rect.width = 240;
+        rect.height = 40;
+    } else if( index == 1 ) {
+        rect.x = ( Graphics.boxWidth / 2 ) - ( 140 );
+        rect.y = 250;
+        rect.width = 240;
+        rect.height = 40;
+    }
+    return rect;
+};
+
+
+function Window_MainMenu_Yes_NoHorzCommand() 
+{
+    this.initialize.apply( this, arguments );
+}
+
+
+Window_MainMenu_Yes_NoHorzCommand.prototype = Object.create( Window_HorzCommand.prototype );
+Window_MainMenu_Yes_NoHorzCommand.prototype.constructor = Window_MainMenu_Yes_NoHorzCommand;
+
+Window_MainMenu_Yes_NoHorzCommand.prototype.initialize = function( x, y ) 
+{
+    Window_HorzCommand.prototype.initialize.call( this, x, y );
+    this.hide();
+}
+
+
+Window_MainMenu_Yes_NoHorzCommand.prototype.makeCommandList = function() 
+{
+    this.addCommand( "Yes, return to main menu!", "command1" );
+    this.addCommand( "No, don't return to main menu", "command2" );
+};
+
+
+Window_MainMenu_Yes_NoHorzCommand.prototype.windowWidth = function() 
 {
     return Graphics.boxWidth;
 }
